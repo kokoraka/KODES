4d5f3086eb3ff19f0c01a332d205872b6feb4b82
Gurisa <gurisadevs@gmail.com>
2017-10-26 23:56:43 +0700

    home, gallery, about

---
+++ assets/custom/js/data.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  var menus = new Array (
+    {
+    'nama' : 'Nasi Lengko Cirebon',
+    'harga' : '12000',
+    'cara' : '',
+    'deskripsi' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'sejarah' : '',
+    'gambar' : ''
+    },
+    {
+    'nama' : 'Rendang Padang',
+    'harga' : '13000',
+    'cara' : '',
+    'deskripsi' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'sejarah' : '',
+    'gambar' : ''
+    },
+    {
+    'nama' : 'Gudeg Yogyakarta',
+    'harga' : '12000',
+    'cara' : '',
+    'deskripsi' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'sejarah' : '',
+    'gambar' : ''
+    }
+  );
+
+
+});
--- assets/custom/js/general.js
+++ assets/custom/js/general.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+  $('.carousel').carousel({
+    interval: 5000,
+    pause: 'hover',
+    direction: 'right'
+  });
+
+  function get_detail(id) {
+
+  }
+  /* check offset width
   var docWidth = document.documentElement.offsetWidth;
 
   [].forEach.call(
@@ -9,74 +20,6 @@ $(document).ready(function() {
       }
     }
   );
+  */
 
-/*
-  $('input').iCheck({
-    checkboxClass: 'icheckbox_square',
-    radioClass: 'iradio_square',
-    increaseArea: '20%'
-  });
-
-  $("#advanced-search-box").hide();
-
-  $("#box-news").mouseover(function() {
-    $("#box-news-title").css("color","#f1f1f1");
-  });
-  $("#box-news").mouseout(function() {
-    $("#box-news-title").css("color","#212121");
-  });
-
-  $("#box-musics").mouseover(function() {
-    $("#box-musics-title").css("color","#f1f1f1");
-  });
-  $("#box-musics").mouseout(function() {
-    $("#box-musics-title").css("color","#212121");
-  });
-
-  $("#box-figures").mouseover(function() {
-    $("#box-figures-title").css("color","#f1f1f1");
-  });
-  $("#box-figures").mouseout(function() {
-    $("#box-figures-title").css("color","#212121");
-  });
-
-  $("#advanced-search-btn").click(function(e){
-    e.preventDefault();
-    $("#advanced-search-box").toggle("slow");
-  });
-
-  $(function() {
-    var sishusearchresult = [
-      "Get", "From", "aJax"
-    ];
-
-    $("#sishu-search-box").autocomplete({
-      source: sishusearchresult
-    });
-
-    var musicsearchresult = [
-      "Ya Tuhanku", "Terpujilah Namamu", "Damai Di Dunia"
-    ];
-
-    $("#music-search-box").autocomplete({
-      source: musicsearchresult
-    });
-  });
-
-  var musicbox = document.getElementById('musicbox');
-  if (musicbox) {
-    CKEDITOR.replace("musicbox", {
-      language: 'id',
-      enterMode: CKEDITOR.ENTER_BR
-    });
-  }
-
-  var sishubox = document.getElementById('sishubox');
-  if (sishubox) {
-    CKEDITOR.replace("sishubox", {
-      language: 'id',
-      enterMode: CKEDITOR.ENTER_BR
-    });
-  }
-*/
 });
